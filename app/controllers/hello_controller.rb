a0070ea53729dfc5c937206b3c7512f99d65a4d5
yonce3 <k0914t_u@icloud.com>
2020-11-08 21:58:36 +0900

    add .

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,13 @@
+class HelloController < ApplicationController
+  def index
+    render plain: 'こんにちは、世界'
+  end
+
+  def view
+    @msg = 'こんにちは、世界'
+  end
+
+  def list
+    @books = Book.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'hello/index', to: 'hello#index'
+  get 'hello/view'
+  get 'hello/list'
 end
